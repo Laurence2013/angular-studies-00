7f5cc25215ea783e0f9f6d86290c1f36b6150ba5
Laurence2013 <lozza244@protonmail.com>
2025-04-14 20:55:37 +0100

    15-commit

--- template-reference-variable-02/src/app/app.component.ts
+++ template-reference-variable-02/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, CommonModule, FormsModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'template-reference-variable-02';
+
+  public title:string = 'Example 3: Referencing a Directive (ngModel)';
+	public userEmail?:string;
+
+	public constructor(){}
 }
