37cc1ccc004e43cf03f097ee3abcc970f972ae88
Laurence2013 <lozza244@protonmail.com>
2025-04-27 09:50:53 +0100

    47-commit

--- host-binding-00/src/app/directives/hover.directive.ts
+++ host-binding-00/src/app/directives/hover.directive.ts
@@ -7,13 +7,15 @@ import { Directive, ElementRef, Renderer2, HostBinding, HostListener } from '@an
 export class HoverDirective {
 
   public constructor(private elRef: ElementRef, private renderer: Renderer2){}
-	@HostBinding('style.backgroundColor') public backgroundColour:string = '#28282B';
+	@HostBinding('style.backgroundColor') public backgroundColour:string = 'blue';
 	@HostBinding('style.border') public border:string = 'red';
 	@HostBinding('style.color') public textColour:string = 'white';
 	@HostListener('mouseenter') public onMouseEnter(){
 		this.backgroundColour = 'green';
+		this.textColour = 'red';
 	}
 	@HostListener('mouseleave') public onMouseLeave(){
 		this.backgroundColour = '#28282B';
+		this.textColour = 'blue';
 	}
 }
